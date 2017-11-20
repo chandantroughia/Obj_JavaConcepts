12bdd900e5c771d9fb236e5431f488a48a51d7c5
Chandan Troughia <chandantroughia.com>
2017-11-19 19:18:59 -0500

    Second Commit

---
+++ interfaces/PPP.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+public class PPP {
+	
+	private static void fact(int n) {
+		if(n <= 1){
+			System.out.println(1);
+			return;
+		}
+		
+		//return n * fact(n-1);
+
+	}
+	
+	public static void main(String[] args) {
+		int n = 1;
+		fact(n);
+	}
+
+}
